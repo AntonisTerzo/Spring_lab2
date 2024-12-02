b0fa736203a6a32d229138272bfe3d59024adc26
AntonisTerzo <antonis.terzopoulos@gmail.com>
2024-12-02 19:26:03 +0100

    Initial commit and started working with the db

---
+++ src/main/java/dev/antonis/spring_lab2/SpringLab2Application.java
@@ -0,0 +1,13 @@
+package dev.antonis.spring_lab2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLab2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringLab2Application.class, args);
+    }
+
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+TRUNCATE TABLE category;
+
+INSERT INTO category(name, symbol, description)
+VALUES ('antonis', '☀️', 'this is a sun');
