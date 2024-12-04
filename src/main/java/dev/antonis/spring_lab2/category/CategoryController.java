97c6ea4497dc259a0023169c5d8b7c189f0da258
AntonisTerzo <antonis.terzopoulos@gmail.com>
2024-12-04 18:21:26 +0100

    finished the findById endpoint

--- src/main/java/dev/antonis/spring_lab2/category/CategoryController.java
+++ src/main/java/dev/antonis/spring_lab2/category/CategoryController.java
@@ -2,6 +2,7 @@ package dev.antonis.spring_lab2.category;
 
 import dev.antonis.spring_lab2.category.dto.CategoryDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -9,7 +10,7 @@ import java.util.List;
 @RestController
 public class CategoryController {
 
-private final CategoryService categoryService;
+    private final CategoryService categoryService;
 
     public CategoryController(CategoryService categoryService) {
         this.categoryService = categoryService;
@@ -18,5 +19,11 @@ private final CategoryService categoryService;
     @GetMapping("/categories")
     public List<CategoryDto> getAllCategories() {
         return categoryService.getAllCategories();
+    }
+
+
+    @GetMapping("/categories/{id}")
+    public CategoryDto getCategoryById(@PathVariable Integer id) {
+        return categoryService.getCategoryById(id);
     }
 }
--- src/main/java/dev/antonis/spring_lab2/category/CategoryService.java
+++ src/main/java/dev/antonis/spring_lab2/category/CategoryService.java
@@ -1,9 +1,12 @@
 package dev.antonis.spring_lab2.category;
 
 import dev.antonis.spring_lab2.category.dto.CategoryDto;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @Service
 public class CategoryService {
@@ -16,5 +19,11 @@ public class CategoryService {
 
     public List<CategoryDto> getAllCategories() {
         return categoryRepository.findAll().stream().map(CategoryDto::fromCategory).toList();
+    }
+
+    public CategoryDto getCategoryById(Integer id)  {
+        return categoryRepository.findById(id)
+                .map(category -> new CategoryDto(category.getName(), category.getSymbol(),category.getDescription()))
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Category not found"));
     }
 }
