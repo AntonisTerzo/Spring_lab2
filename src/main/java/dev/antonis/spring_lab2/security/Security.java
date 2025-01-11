b3a75b02ad37d5411e02c5c312075e16814ac36f
AntonisTerzo <antonis.terzopoulos@gmail.com>
2025-01-11 23:56:38 +0100

    added security and Oauth2 plus jwt

---
+++ src/main/java/dev/antonis/spring_lab2/security/Security.java
@@ -0,0 +1,65 @@
+package dev.antonis.spring_lab2.security;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.http.HttpMethod.*;
+
+@Configuration
+@EnableMethodSecurity
+public class Security {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorize ->
+                        authorize
+                                .requestMatchers(GET, "/api/categories").permitAll()
+                                .requestMatchers(GET, "/api/categories/").permitAll()
+                                .requestMatchers(POST, "/api/categories").hasRole("ADMIN")
+                                .requestMatchers(GET, "/api/locations/public").permitAll()
+                                .requestMatchers(GET, "/api/locations/*").permitAll()
+                                .requestMatchers(GET, "api/locations/categories/").permitAll()
+                                .requestMatchers(GET, "/api/locations/area").permitAll()
+                                .requestMatchers(GET, "/api/locations/users/*").hasRole("USER")
+                                .requestMatchers(POST, "/api/locations/add").hasRole("USER")
+                                .requestMatchers(PUT, "/api/locations/").hasRole("USER")
+                                .requestMatchers(DELETE, "/api/locations").hasRole("USER")
+                                .requestMatchers(
+                                        "/v3/api-docs",
+                                        "/v3/api-docs/**",
+                                        "/swagger-resources",
+                                        "/swagger-resources/**",
+                                        "/configuration/ui",
+                                        "/configuration/security",
+                                        "/swagger-ui/**",
+                                        "/webjars/**",
+                                        "/swagger-ui.html").permitAll()
+                                .requestMatchers("/error").permitAll()
+                                .anyRequest().denyAll())
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter())))
+                .exceptionHandling(exception ->
+                        exception.authenticationEntryPoint(new CustomAuthenticationEntryPoint()));
+        return http.build();
+    }
+    @Bean
+    public JwtAuthenticationConverter jwtAuthenticationConverter() {
+        JwtGrantedAuthoritiesConverter grantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+        grantedAuthoritiesConverter.setAuthorityPrefix("");
+        grantedAuthoritiesConverter.setAuthoritiesClaimName("scope");
+
+        JwtAuthenticationConverter jwtConverter = new JwtAuthenticationConverter();
+        jwtConverter.setJwtGrantedAuthoritiesConverter(grantedAuthoritiesConverter);
+        return jwtConverter;
+    }
+    }
